682b6080a3c7afa62f28c7d25f3d3c4352487365
Mou-Chun Wang <jimmyexx@gmail.com>
2017-12-01 09:49:20 +0800

    reaching requirements

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,25 @@
 $(function() {
-
+  console.log(0946);
   // your code will go here
+  var myReportCard = 'https://www.codeschool.com/users/mjwang.json';
+  $.ajax({
+    url: myReportCard,
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      $.each(response.courses.completed, function(key, value){
+        //currentBadges = value.badge;
+        value.badge = '<div class="course">' + value.badge + '</div>';
+        value.title = '<h3>' + value.title + '</h3>';
+        value.img = value.badge;
+      
+        value.a = '<a href="' + value.url + '" target="_blank" class="btn btn-primary">';
+        console.log(value);
+
+        //console.log(value.badge);
+      });
+
+    }
+  });
 
 });
